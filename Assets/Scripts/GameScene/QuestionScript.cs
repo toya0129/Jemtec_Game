2fdb48dd67e53372dc943356c733f9a3cd70f2b3
toya0129 <citu15059@g.nihon-u.ac.jp>
2019-08-19 03:34:40 +0900

    add game scene UI

---
+++ Assets/Scripts/GameScene/QuestionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionScript : MonoBehaviour
+{
+    [System.SerializableAttribute]
+    public class ValueList
+    {
+        public List<GameObject> List = new List<GameObject>();
+
+        public ValueList(List<GameObject> list)
+        {
+            List = list;
+        }
+    }
+
+    [SerializeField]
+    private List<ValueList> panel = new List<ValueList>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
