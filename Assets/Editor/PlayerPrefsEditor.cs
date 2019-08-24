c78601da55fd9956b702206854597bd9c0294f8d
Cherenco <cida14055@g.nihon-u.ac.jp>
2019-08-24 17:30:15 +0900

    add scene transition add prototype for each screen add save data locally

---
+++ Assets/Editor/PlayerPrefsEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using PlayerPrefs = PreviewLabs.PlayerPrefs;
+
+public class PlayerPrefsEditor : MonoBehaviour
+{
+    [MenuItem("PlayerPrefs/DeleteAll")]
+	public static void DeleteAll(){
+		PlayerPrefs.DeleteAll ();
+		PlayerPrefs.Flush ();
+		Debug.Log("Delete All Data of PlayerPrefs");
+	}
+}
---
+++ Assets/Scripts/Ranking/Ranking.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayerPrefs = PreviewLabs.PlayerPrefs;
+
+public class Ranking : MonoBehaviour
+{
+    [SerializeField]
+    List<Text> rankingTextList = new List<Text>();
+    void Start()
+    {
+        for(int i = 0; i < 5; i++){
+            this.rankingTextList[i].text = PlayerPrefs.GetInt("ranking" + i.ToString(), 0).ToString();
+        }
+    }
+}
